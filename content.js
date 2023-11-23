2babcd201bace4318060302c0dc86ca2102c9bd2
Ростислав Решетар <u1-911@office107.local>
2023-11-23 19:29:27 +0200

    pushes added

--- content.js
+++ content.js
@@ -1,18 +1,19 @@
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const extractButton = document.getElementById('extractButton');
 
     extractButton.addEventListener('click', function () {
-        console.log('Button clicked')
 
         const radioTemplate = getRadiobutton()
         const allLangText = document.getElementById('textInput').value;
         const projectLang = getProjectLang(radioTemplate)
         
 
-       runScript(allLangText, projectLang, radioTemplate);
-
-       chrome.runtime.onMessage.addListener((resultText) => {
-            result = resultText.resultText
+        runScript(allLangText, projectLang, radioTemplate)
+        chrome.runtime.onMessage.addListener((resultText) => {
+            let result = resultText.resultText
             const extractList = document.getElementById('projectList');
             const otherProblems = document.getElementById('OtherProblems')
             const titleList = document.getElementById('titleList');
@@ -61,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     const li = document.createElement('li');
                     li.textContent = result.imageProblem[i]
                     li.style.color = 'red'
-    
+                    
                     otherProblems.appendChild(li)
                 }
             }
@@ -81,25 +82,48 @@ document.addEventListener('DOMContentLoaded', function () {
                 return values.every(val => val === firstValue );
             };
 
+
+            
             let checkGlobalTemplateNumLen = areAllValuesSameLength(nmbProblem.templateText.globalText)
             let checkSideTemplateNumLen = areAllValuesSameLength(nmbProblem.templateText.sideText)
             let checkGlobalTemplateNumVal = areAllValuesSame(nmbProblem.templateText.globalText)
             let checkSideTemplateNumLVal = areAllValuesSame(nmbProblem.templateText.sideText)
 
+            processNumbersProblem()
+            
+            function processNumbersProblem() {
+                //Вимбачте за гівнокод
+                if(!checkGlobalTemplateNumLen) {
+                    for(const key in nmbProblem.templateText.globalText) {
+                        const li = document.createElement('li');
+                        li.style.color = 'red'
+                        li.textContent = `На ${key.toUpperCase()} мові ${nmbProblem.templateText.globalText[key].length} цифрових символів(Головний текст)`
+                        otherProblems.appendChild(li)
+                    }
+                    
+                } else if(!checkSideTemplateNumLen) {
+                    for(const key in nmbProblem.templateText.sideText) {
+                        const li = document.createElement('li');
+                        li.style.color = 'red'
+                        li.textContent = `На ${key.toUpperCase()} мові ${nmbProblem.templateText.sideText[key].length} цифрових символів(Текст кнопки або тайтла)`
+                        otherProblems.appendChild(li)
+                    }
+                }
 
-            if(!checkGlobalTemplateNumLen || !checkSideTemplateNumLen) {
-                for(const key in nmbProblem.templateText.globalText) {
-                    const li = document.createElement('li');
+                if (!checkGlobalTemplateNumVal) {
+                    const li = document.createElement('li');    
+                    let incorectVal = findKeyValuesNotEqualToMajority(nmbProblem.templateText.globalText)
                     li.style.color = 'red'
-                    li.textContent = `На ${key.toUpperCase()} мові ${nmbProblem.templateText.globalText[key].length} цифрових символів`
+                    li.textContent = `Значення цифр сходяться не всюди(дата, ставка, призові і т. д.) на мовах: ${Object.keys(incorectVal)} (Головний текст)`
+                    otherProblems.appendChild(li)
+                } else if (!checkSideTemplateNumLVal) {
+                    const li = document.createElement('li');    
+                    let incorectVal = findKeyValuesNotEqualToMajority(nmbProblem.templateText.sideText)
+                    li.style.color = 'red'
+                    li.textContent = `Значення цифр сходяться не всюди(дата, ставка, призові і т. д.) на мовах: ${Object.keys(incorectVal)} (Текст кнопки або тайтла)`
                     otherProblems.appendChild(li)
                 }
-            } else if (!checkGlobalTemplateNumVal || !checkSideTemplateNumLVal) {
-                const li = document.createElement('li');    
-                let incorectVal = findKeyValuesNotEqualToMajority(nmbProblem.templateText.globalText)
-                li.style.color = 'red'
-                li.textContent = `Значення цифр сходяться не всюди(дата, ставка, призові і т. д.) на мовах: ${Object.keys(incorectVal)}`
-                otherProblems.appendChild(li)
+
             }
 
             function findKeyValuesNotEqualToMajority(obj) {
@@ -125,9 +149,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
        })
     });
-
-
-
+    
     function runScript(allLangText, projectLang, radioTemplate) {
         if (allLangText) {
             chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
@@ -143,29 +165,29 @@ document.addEventListener('DOMContentLoaded', function () {
                         
                         //console.log(templateText)
                         //console.log(exelText)
-                        console.log(radioTemp)
-
-
+                        //console.log(radioTemp)
+    
+    
                         let resultText = compareAllTexts(templateText, exelText)
                         chrome.runtime.sendMessage({ resultText });
-
+    
                         function getProjectName() {
                             let dropDownElement = this.document.querySelector('.chosen-single')
                             let projectName = dropDownElement.querySelector('span')
                         
                             return projectName.innerText
                         }
-
+    
                         function getTextFromMelica() {
                             let crmLangElements = {globalText: {}, sideText: {}}
                             let project = getProjectName()
-
+    
                             if(radioTemp === "telegramTemplate") {
                                 return processTelegramTemplate()
                             } else if (radioTemp === "pushTemplate") {
                                 return processPushTemplate()
                             }
-
+    
                             function processTelegramTemplate() {
                                 for (let i = 0; i < projectLang[project].length; i++) {
                                     const langKey = projectLang[project][i];
@@ -177,7 +199,7 @@ document.addEventListener('DOMContentLoaded', function () {
                                 }
                                 return crmLangElements
                             }
-
+    
                             function processPushTemplate() {
                                 for (let i = 0; i < projectLang[project].length; i++) {
                                     const langKey = projectLang[project][i];
@@ -190,15 +212,37 @@ document.addEventListener('DOMContentLoaded', function () {
                                     crmLangElements.globalText[langKey] = textTab;
                                     crmLangElements.sideText[langKey] = textSide;
                                 }
+                                crmLangElements.sideText = processUserNameTitle(crmLangElements.sideText) 
+                                
                                 return crmLangElements
                             }
+    
+                            function processUserNameTitle(titleText) {
+                                
+                                for(let key in titleText) {
+                                    if(titleText[key].toLowerCase().includes('user_name_ru')) alert('Присутній user_name_ru')
+    
+                                    if(titleText[key].includes('[') && titleText[key].includes(']')) {
+                                        let lapki = []
+                                        let parametr1 = titleText[key].slice(titleText[key].indexOf(']') - 1, titleText[key].indexOf(']'))
+                                        let parametr2 = titleText[key].slice(titleText[key].indexOf(']') + 1, titleText[key].length)
+    
+                                        lapki.push(parametr1 + parametr2)
+                                        
+                                        //console.log(lapki.join('').toString())
+                                        titleText[key] = lapki.join('').toString()
+                                    }
+                                }
+                                return titleText
+                            }
                         }
-
+    
+    
                         function getExelString(inputString) {
                             const project = getProjectName()
                             const rows = inputString.trim().split('\n');
                             const extendRes = {mainText: {}, sideText: {}}
-
+    
                             let mTextArr = []
                             let btnTextArr = []
                         
@@ -212,23 +256,35 @@ document.addEventListener('DOMContentLoaded', function () {
                                     }
                                 }
                             }
-
+    
                             mText = mTextArr.filter(item => item.trim() !== '')
                             btnText = btnTextArr.filter(item => item.trim() !== '')
-
+    
                             for(let i = 0; i < Object.keys(projectLang[project]).length; i++) {
-                                extendRes.mainText[projectLang[project][i]] = mText[i]
-                                extendRes.sideText[projectLang[project][i]] = btnText[i]
-
-                                if(extendRes.mainText[projectLang[project][i]] === undefined) {
-                                    extendRes.mainText[projectLang[project][i]] = extendRes.mainText['en']
-                                    extendRes.sideText[projectLang[project][i]] = extendRes.sideText['en']
+                                const langKeys = projectLang[project][i]
+    
+                                extendRes.mainText[langKeys] = mText[i]
+                                extendRes.sideText[langKeys] = btnText[i]
+    
+                                if(extendRes.mainText[langKeys] === undefined) {
+                                    extendRes.mainText[langKeys] = extendRes.mainText['en']
+                                    extendRes.sideText[langKeys] = extendRes.sideText['en']
                                 }
                             }
-                            console.log(extendRes)
+    
+                            
+    
+                            if(radioTemp === "pushTemplate") {
+                                let tempMain = extendRes.sideText
+                                let tempSide = extendRes.mainText
+                                
+                                extendRes.mainText = tempMain
+                                extendRes.sideText = tempSide
+                            }
+    
                             return extendRes;
                         }
-
+    
                         function stripHtmlTagsAndSpaces(stringToFilter) {
                             // Remove HTML tags
                             const stringWithoutTags = stringToFilter.replace(/<[^>]*>/g, '');
@@ -241,42 +297,62 @@ document.addEventListener('DOMContentLoaded', function () {
                         
                             // Remove extra double quotes
                             const sanitizedString = trimmedString.replace(/"/g, '').trim();
+    
+    
+                            const totalfullfinalyaaa = sanitizedString.replace('amp;', '')
                         
-                            return sanitizedString;
+                            return totalfullfinalyaaa;
                         }
-
+    
                         function getProblemsWithImage() {
                             let project = getProjectName()
                             let problems = []
-                            for(let i = 0; i < Object.keys(projectLang[project]).length; i++) {
-                                let imageLink = document.getElementById(`img_name_${projectLang[project][i]}`).value
-                                let divButton = document.getElementById(`buttons_${projectLang[project][i]}`)
-
-
-                                if(imageLink.trim() === "") {                 
-                                    problems.push(`Відсутній лінк на картинку на ${projectLang[project][i].toUpperCase()} мові`) 
+                            if(radioTemp === "telegramTemplate") {
+                                for(let i = 0; i < Object.keys(projectLang[project]).length; i++) {
+                                    let imageLink = document.getElementById(`img_name_${projectLang[project][i]}`).value
+                                    let divButton = document.getElementById(`buttons_${projectLang[project][i]}`)
+    
+                                    if(imageLink.trim() === "") {                 
+                                        problems.push(`Відсутній лінк на картинку на ${projectLang[project][i].toUpperCase()} мові`) 
+                                    }
+    
+                                    if(!(divButton.innerHTML.trim() === "")) {
+                                        let buttonLink = divButton.querySelectorAll('input[type="text"]')[1].value
+    
+                                        if(buttonLink === "") {
+                                            problems.push(`Відсутнє посилання під кнопкою на ${projectLang[project][i].toUpperCase()} мові`) 
+                                        } 
+                                                   
+                                    } else {
+                                        problems.push(`Дів з кнопкою пустий на ${projectLang[project][i].toUpperCase()} мові`) 
+    
+                                    }
                                 }
-
-                                if(!(divButton.innerHTML.trim() === "")) {
-                                    let buttonLink = divButton.querySelectorAll('input[type="text"]')[1].value
-
-                                    if(buttonLink === "") {
-                                        problems.push(`Відсутнє посилання під кнопкою на ${projectLang[project][i].toUpperCase()} мові`) 
-                                    } 
-                                               
-                                } else {
-                                    problems.push(`Дів з кнопкою пустий на ${projectLang[project][i].toUpperCase()} мові`) 
-
+                            } else if(radioTemp === "pushTemplate") {
+                                let iconLink = document.getElementById(`icon_name`).value
+                                let imgInk = document.getElementById(`img_name`).value
+                                let linkLINK = document.getElementById(`link`).value
+    
+                                if(iconLink.trim() === "") {
+                                    problems.push(`Відсутній лінк на іконку`)
+                                } 
+    
+                                if(imgInk.trim() === "") {
+                                    problems.push(`Відсутній лінк на картинку`)
                                 }
-
+    
+                                if(linkLINK.trim() === '') {
+                                    problems.push(`Відсутній лінк`)
+                                }
+                                
                             }
                             return problems
                         }
-
+    
                         function getAllNumbers() {
                             const templateText = getTextFromMelica()
                             const exelText = getExelString(allLangText)
-
+    
                             function removeNonNumericSymbolsAndReturnLength(obj) {
                                 for (let key in obj) {
                                     if (typeof obj[key] === 'string') {
@@ -285,18 +361,18 @@ document.addEventListener('DOMContentLoaded', function () {
                                     }
                                 }
                             }
-
+    
                             removeNonNumericSymbolsAndReturnLength(templateText.globalText)
                             removeNonNumericSymbolsAndReturnLength(exelText.mainText)
                             removeNonNumericSymbolsAndReturnLength(templateText.sideText)
                             removeNonNumericSymbolsAndReturnLength(exelText.sideText)
-
-
+    
+    
                              return {templateText, exelText}
                         }
-
+    
                         function compareAllTexts(MelicaText, ExelText) {
-
+    
                             let comparison = {
                                 resultOfComparison: {}, 
                                 project: getProjectName(), 
@@ -321,21 +397,21 @@ document.addEventListener('DOMContentLoaded', function () {
                                     comparison.textForComparison.MelicaTxt.sideText[key] = MelicaTextSideValue
                                     comparison.textForComparison.exelTxt.mainText[key] = exelTextValue
                                     comparison.textForComparison.exelTxt.sideText[key] = exelTextSideValue
-
+    
                                 } else{
                                     comparison.resultOfComparison[key] = `На ${key.toUpperCase()} мові все ок`
                                     comparison.textForComparison.MelicaTxt.mainText[key] = MelicaTextValue
                                     comparison.textForComparison.MelicaTxt.sideText[key] = MelicaTextSideValue
                                     comparison.textForComparison.exelTxt.mainText[key] = exelTextValue
                                     comparison.textForComparison.exelTxt.sideText[key] = exelTextSideValue
-
+    
                                 }
                             }
                             
-
+    
                             return comparison          
                         }
-
+    
                 
                     },
                     args: [allLangText, projectLang, radioTemplate]            
@@ -345,7 +421,7 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             alert('Input field is empty.');
         }
-
+    
         
     }
 
